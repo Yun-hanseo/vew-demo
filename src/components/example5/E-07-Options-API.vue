df634174f2720bc96bbd27d21f4fe0cf73885c84
galmegi2 <gogodoom59@gmail.com>
2025-11-03 09:27:42 +0900

    READE ME

--- src/components/example5/E-07-Options-API.vue
+++ src/components/example5/E-07-Options-API.vue
@@ -11,61 +11,80 @@
 </template>
 
 <script>
-import { ref, computed, watch, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
-
 export default {
-  name: 'E07CompositionApi',
+  name: 'E07OptionsApi',
+
   props: {
     title: {
       type: String,
       default: 'User Information'
     }
   },
-  setup(props) {
-    const firstName = ref('John')
-    const lastName = ref('Doe')
-    const greetCount = ref(0)
-    const message = ref('')
 
-    // ✅ computed property
-    const fullName = computed(() => `${firstName.value} ${lastName.value}`)
-
-    // ✅ methods
-    const greet = () => {
-      greetCount.value++
-      message.value = `Hello, ${fullName.value}!`
-    }
-
-    const resetGreetCount = () => {
-      greetCount.value = 0
-    }
-
-    watch(greetCount, (newValue, oldValue) => {
-      console.log(`Greet count changed from ${oldValue} to ${newValue}`)
-      if (newValue >= 3) {
-        message.value = "That's enough greetings for now!"
-      }
-    })
-
-    console.log('beforeCreate hook (runs at setup)')
-    onBeforeMount(() => console.log('beforeMount hook'))
-    onMounted(() => console.log('mounted hook'))
-    onBeforeUpdate(() => console.log('beforeUpdate hook'))
-    onUpdated(() => console.log('updated hook'))
-    onBeforeUnmount(() => console.log('beforeUnmount hook'))
-    onUnmounted(() => console.log('unmounted hook'))
-
+  data() {
     return {
-      firstName,
-      lastName,
-      greetCount,
-      message,
-      fullName,
-      greet,
-      resetGreetCount,
-      props
-    }
-  }
-}
-</script>
+      firstName: 'John',
+      lastName: 'Doe',
+      greetCount: 0,
+      message: ''
+    };
+  },
 
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  },
+
+  methods: {
+    greet() {
+      this.greetCount++;
+      this.message = `Hello, ${this.fullName}!`;
+    },
+    resetGreetCount() {
+      this.greetCount = 0;
+    }
+  },
+
+  watch: {
+    greetCount(newValue, oldValue) {
+      console.log(`Greet count changed from ${oldValue} to ${newValue}`);
+      if (newValue >= 3) {
+        this.message = "That's enough greetings for now!";
+      }
+    }
+  },
+
+  beforeCreate() {
+    console.log('beforeCreate hook');
+  },
+
+  created() {
+    console.log('created hook');
+  },
+
+  beforeMount() {
+    console.log('beforeMount hook');
+  },
+
+  mounted() {
+    console.log('mounted hook');
+  },
+
+  beforeUpdate() {
+    console.log('beforeUpdate hook');
+  },
+
+  updated() {
+    console.log('updated hook');
+  },
+
+  beforeUnmount() {
+    console.log('beforeUnmount hook');
+  },
+
+  unmounted() {
+    console.log('unmounted hook');
+  }
+};
+</script>
